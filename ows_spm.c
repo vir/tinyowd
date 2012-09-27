4163ffc24e26952a329f9dfe810a2d0d61bf416e
Vasily Redkin <vir@ctm.ru>
2012-09-27 22:19:15 +0400

    Removed a chunk of unused code

--- ows_spm.c
+++ ows_spm.c
@@ -8,8 +8,6 @@
 
 #define PGM_PAGE_SIZE 32
 
-void boot_program_page(uint32_t page, uint8_t *buf);
-
 static void write_page(uint16_t addr)
 {
 	uint8_t sreg;
@@ -71,40 +69,4 @@ void ows_spm()
 		break;
 	}
 }
-
-void boot_program_page(uint32_t page, uint8_t *buf)
-{
-
-	uint16_t i;
-	uint8_t sreg;
-
-	// Disable interrupts.
-
-	sreg = SREG;
-	cli();
-
-	eeprom_busy_wait ();
-
-	boot_page_erase (page);
-	boot_spm_busy_wait ();      // Wait until the memory is erased.
-
-	for (i=0; i<SPM_PAGESIZE; i+=2)
-	{
-		// Set up little-endian word.
-		uint16_t w = *buf++;
-		w += (*buf++) << 8;
-		boot_page_fill (page + i, w);
-	}
-
-	boot_page_write (page);     // Store buffer in flash page.
-	boot_spm_busy_wait();       // Wait until the memory is written.
-
-	// Reenable RWW-section again. We need this if we want to jump back
-	// to the application after bootloading.
-	//boot_rww_enable ();
-
-	// Re-enable interrupts (if they were ever enabled).
-	SREG = sreg;
-}
-
 
